c0e59b643d68e74f6b74e8887b45f4750e83c861
Julio Vitor <juliovitor99@outlook.com>
2026-02-14 16:47:00 -0300

    mudança do copund para llama

--- Agents/funções_conversa_simples.py
+++ Agents/funções_conversa_simples.py
@@ -12,7 +12,7 @@ def criando_agentes(idioma : str, função :str, nome: str) -> tuple:
         name= nome,
         system_message=(f'Você vai responder sempre no idioma {idioma} e será {função}'),
         llm_config={
-            "model": "groq/compund",
+            "model": "llama-3.1-8b-instant",
             
             "api_key": os.getenv("GROQ_API_KEY"),
             "api_type": "groq",
