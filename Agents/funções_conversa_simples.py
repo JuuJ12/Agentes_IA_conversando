bbc57c72136cc004c90207aa37862fff59557075
Julio Vitor <juliovitor99@outlook.com>
2026-08-24 10:31:03 -0300

    mudança de modelo llama -> gpt

--- Agents/funções_conversa_simples.py
+++ Agents/funções_conversa_simples.py
@@ -12,7 +12,7 @@ def criando_agentes(idioma : str, função :str, nome: str) -> tuple:
         name= nome,
         system_message=(f'Você vai responder sempre no idioma {idioma} e será {função}'),
         llm_config={
-            "model": "llama-3.1-8b-instant",
+            "model": "openai/gpt-oss-120bt",
             
             "api_key": os.getenv("GROQ_API_KEY"),
             "api_type": "groq",
